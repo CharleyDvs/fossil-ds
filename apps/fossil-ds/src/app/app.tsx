279fa0c4e4b7705369509a61b2962a847cf3993a
Carlos D. Jaime <ing.carlosjaime@outlook.com>
2021-08-19 12:49:41 -0500

    Add a documentation page for the main app

--- apps/fossil-ds/src/app/app.tsx
+++ apps/fossil-ds/src/app/app.tsx
@@ -1,54 +1,11 @@
-import { Suspense, lazy } from 'react'
 import styles from './app.module.scss'
-import { Switch, Route, useParams } from 'react-router-dom'
-import { ErrorBoundary } from 'react-error-boundary'
+import { Switch, Route } from 'react-router-dom'
 import { SideMenu, ThemeSwitch } from '../layout'
-
-interface ErrorFallbackProps {
-  error: Error
-  resetErrorBoundary?: () => void
-}
-interface ParamsProps {
-  componentName: string
-}
+import { Documentation } from '../pages'
 
 const Home = () => <h1>Hello World</h1>
 
-const ErrorFallback = ({
-  error,
-  resetErrorBoundary,
-}: ErrorFallbackProps): JSX.Element => {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button
-        onClick={() => {
-          if (resetErrorBoundary) resetErrorBoundary()
-        }}
-      >
-        Try again
-      </button>
-    </div>
-  )
-}
-
 export function App() {
-  const Example = () => {
-    const { componentName } = useParams<ParamsProps>()
-    const Component = (componentName: string) =>
-      lazy(() => import(`!babel-loader!@mdx-js/loader!./${componentName}.mdx`))
-    const Documentation = Component(componentName)
-
-    return (
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Documentation />
-        </Suspense>
-      </ErrorBoundary>
-    )
-  }
-
   return (
     <div className={styles.app}>
       <SideMenu />
@@ -58,7 +15,7 @@ export function App() {
         </Route>
         <Route path="/components/:componentName">
           <h1>Component</h1>
-          <Example />
+          <Documentation />
         </Route>
         <Route exact path="/colors">
           <h1>Color</h1>
