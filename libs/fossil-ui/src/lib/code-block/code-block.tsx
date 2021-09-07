badc603a432fa3b18a24a44011c51b6544eed197
Carlos D. Jaime <ing.carlosjaime@outlook.com>
2021-09-06 19:35:08 -0500

    Add a button to copy to clipboard at Documentation component

--- libs/fossil-ui/src/lib/code-block/code-block.tsx
+++ libs/fossil-ui/src/lib/code-block/code-block.tsx
@@ -1,67 +1,73 @@
 import { useState } from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import cx from 'classnames'
-import { makeStyles } from '@material-ui/core/styles'
+import { IconButton, Tooltip } from '@material-ui/core'
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 import { HiTemplate, HiCode, HiClipboard } from 'react-icons/hi'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { tokens } from '@fossil-ds/shared/styles'
 
 interface CodeBlockProps {
   children: string
   component?: React.ReactNode
 }
 
-const useStyles = makeStyles({
-  container: {
-    position: 'relative',
-  },
-  previewContainer: {
-    border: '1px solid #d3d3d3',
-    borderRadius: '16px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    minHeight: '300px',
-  },
-  controlsContainer: {
-    position: 'absolute',
-    right: '16px',
-    bottom: '16px',
-  },
-  icon: {
-    cursor: 'pointer',
-    opacity: '.3',
-    margin: '0 8px',
-    transform: 'scale(1.5)',
-    transition: 'all ease .3s',
-    '&:hover': {
-      transform: 'scale(1.7)',
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      position: 'relative',
     },
-  },
-  clipboardBtn: {
-    position: 'absolute',
-    bottom: '16px',
-    left: '16px',
-    '&:hover': {
-      opacity: '1',
+    previewContainer: {
+      border: `1px solid ${tokens.grey[20].value}`,
+      borderRadius: '16px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minHeight: '300px',
     },
-  },
-  isSelected: {
-    opacity: '1',
-  },
-  black: {
-    color: '#222',
-  },
-  white: {
-    color: '#dfdfdf',
-  },
-})
+    controlsContainer: {
+      position: 'absolute',
+      right: theme.spacing(2),
+      bottom: theme.spacing(2),
+    },
+    icon: {
+      cursor: 'pointer',
+      margin: '0 8px',
+    },
+    clipboardBtn: {
+      position: 'absolute',
+      bottom: theme.spacing(1),
+      left: '0',
+      '&:hover': {
+        opacity: '1 !important',
+      },
+    },
+    notSelected: {
+      opacity: '.5',
+    },
+    black: {
+      color: tokens.grey[100].value,
+    },
+    white: {
+      color: tokens.grey[20].value,
+    },
+  }),
+)
 
 export const CodeBlock = ({
   children,
   component,
 }: CodeBlockProps): JSX.Element => {
   const [showCode, setShowCode] = useState(true)
+  const [clipboardText, setClipboardText] = useState('copy to clipboard')
   const styles = useStyles()
+
+  const handleCopy = () => {
+    setClipboardText('copied to the clipboard!')
+    setTimeout(() => {
+      setClipboardText('copy to clipboard')
+    }, 3000)
+  }
 
   return (
     <div className={styles.container}>
@@ -74,7 +80,7 @@ export const CodeBlock = ({
                 style={{
                   borderRadius: '16px',
                   margin: '0 auto',
-                  padding: '20px',
+                  padding: '16px',
                   minHeight: '300px',
                   ...style,
                 }}
@@ -90,8 +96,12 @@ export const CodeBlock = ({
             )}
           </Highlight>
           <div className={cx(styles.clipboardBtn, styles.white, styles.icon)}>
-            <CopyToClipboard text={children}>
-              <HiClipboard />
+            <CopyToClipboard text={children} onCopy={handleCopy}>
+              <Tooltip title={clipboardText} placement="top">
+                <IconButton className={cx(styles.clipboardBtn, styles.white)}>
+                  <HiClipboard />
+                </IconButton>
+              </Tooltip>
             </CopyToClipboard>
           </div>
         </>
@@ -99,20 +109,30 @@ export const CodeBlock = ({
         <div className={styles.previewContainer}>{component}</div>
       )}
       <div className={styles.controlsContainer}>
-        <HiTemplate
-          className={cx(
-            styles.icon,
-            !showCode ? [styles.isSelected, styles.black] : styles.white,
-          )}
-          onClick={() => setShowCode(false)}
-        />
-        <HiCode
-          className={cx(
-            styles.icon,
-            showCode ? [styles.isSelected, styles.white] : styles.black,
-          )}
-          onClick={() => setShowCode(true)}
-        />
+        <Tooltip title="show component" placement="top">
+          <IconButton
+            className={cx(
+              showCode ? [styles.notSelected, styles.white] : styles.black,
+              styles.icon,
+            )}
+            aria-label="show component"
+            onClick={() => setShowCode(false)}
+          >
+            <HiTemplate />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="show code snippet" placement="top">
+          <IconButton
+            className={cx(
+              showCode ? styles.white : [styles.black, styles.notSelected],
+              styles.icon,
+            )}
+            aria-label="show code"
+            onClick={() => setShowCode(true)}
+          >
+            <HiCode />
+          </IconButton>
+        </Tooltip>
       </div>
     </div>
   )
